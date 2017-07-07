b0d0fd14657d943b4a011bb7b5c2302c738e6e77
sbt-kazakov-ag <antonanton08@gmail.com>
2017-07-07 19:41:55 +0300

    close #2 close #3 close #4 close #5

---
+++ app/src/main/java/com/antonkazakov/todo/data/AppComponent.kt
@@ -0,0 +1,13 @@
+package com.antonkazakov.todo.data
+
+import dagger.Component
+
+/**
+ * @author Anton Kazakov
+ * @date 07.07.17.
+ */
+@Component(modules = arrayOf(AppModule::class))
+interface AppComponent {
+
+
+}
---
+++ app/src/main/java/com/antonkazakov/todo/data/repository/MyDatabaseOpenHelper.kt
@@ -0,0 +1,44 @@
+package com.antonkazakov.todo.data.repository
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import org.jetbrains.anko.db.*
+
+/**
+ * @author Anton Kazakov
+ * @date 07.07.17.
+ */
+
+private val VERSION = 1
+private val NAME = "TasksDatabase"
+
+class MyDatabaseOpenHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, NAME, null, VERSION) {
+
+    /**
+     * Lazy loading BD instance
+     */
+    companion object {
+        private var instance: MyDatabaseOpenHelper? = null
+
+        @Synchronized
+        fun getInstance(ctx: Context): MyDatabaseOpenHelper {
+            if (instance == null) {
+                instance = MyDatabaseOpenHelper(ctx)
+            }
+            return instance!!
+        }
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.createTable("User", true,
+                "id" to INTEGER + PRIMARY_KEY,
+                "name" to TEXT,
+                "photo" to BLOB)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        // Here you can upgrade tables, as usual
+        db.dropTable("User", true)
+    }
+}
+
