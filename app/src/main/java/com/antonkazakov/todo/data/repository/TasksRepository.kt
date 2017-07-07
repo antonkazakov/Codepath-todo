aac192aa4640b5a3119853ee62eb6fedcf0eed15
sbt-kazakov-ag <antonanton08@gmail.com>
2017-07-07 19:54:37 +0300

    close #6

--- app/src/main/java/com/antonkazakov/todo/data/repository/TasksRepository.kt
+++ app/src/main/java/com/antonkazakov/todo/data/repository/TasksRepository.kt
@@ -2,6 +2,8 @@ package com.antonkazakov.todo.data.repository
 
 import com.antonkazakov.todo.data.beans.Task
 import org.jetbrains.anko.db.classParser
+import org.jetbrains.anko.db.parseList
+import org.jetbrains.anko.db.parseSingle
 import org.jetbrains.anko.db.select
 import javax.inject.Inject
 
@@ -9,13 +11,23 @@ import javax.inject.Inject
  * @author Anton Kazakov
  * @date 07.07.17.
  */
-class TasksRepository @Inject constructor(val databaseOpenHelper: MyDatabaseOpenHelper) {
+class TasksRepository @Inject constructor(val databaseOpenHelper: MyDatabaseOpenHelper) : ITasksRepository {
 
-    fun test(): List<Task> {
+    override fun getTasks(): List<Task>? {
         return databaseOpenHelper.use {
-            select("User", "name")
-                    .parseList(classParser<Task>())
+            select("Tasks")
+                    .exec { parseList(classParser<Task>()) }
+
         }
     }
+
+    override fun getTaskById(id: Long): Task? {
+        return databaseOpenHelper.use {
+            select("Tasks")
+                    .whereArgs("(id = {id}", "id" to id)
+                    .exec { parseSingle(classParser<Task>()) }
+        }
+    }
+
 
 }
